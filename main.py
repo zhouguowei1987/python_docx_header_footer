ce2a28cd47f85c350a62c21b448055a33ce399c2
729044963@qq.com <729044963@qq.com>
2023-03-07 16:38:39 +0800

    从根目录循环

--- main.py
+++ main.py
@@ -16,6 +16,28 @@ def remove_header_footer(doc, finish_doc):
     document.save(finish_doc)
 
 
+def get_word_pages(in_file):
+    pages = 1
+    try:
+        word = wc.Dispatch("Word.Application")
+        try:
+            doc = word.Documents.Open(in_file)
+            word.ActiveDocument.Repaginate()
+            pages = word.ActiveDocument.ComputeStatistics(2)
+            print(pages)
+            doc.Close()
+            word.Quit()
+            return pages
+        except Exception as e:
+            print(e)
+        finally:
+            return pages
+    except Exception as e:
+        print(e)
+    finally:
+        return pages
+
+
 def doc2docx(in_file, out_file):
     try:
         word = wc.Dispatch("Word.Application")
@@ -33,31 +55,40 @@ def doc2docx(in_file, out_file):
         print(e)
 
 
-word_dir = "G:\\www.ychedu.com\\母婴育儿"
-finish_dir = "G:\\www.ychedu.com\\母婴育儿_finish"
-doc2docx_dir = "G:\\www.ychedu.com\\母婴育儿_doc2docx"
-
 if __name__ == '__main__':
-    if not os.path.exists(finish_dir):
-        os.mkdir(finish_dir)
 
-    if not os.path.exists(doc2docx_dir):
-        os.mkdir(doc2docx_dir)
+    subject_dirs_arr = ['化学试卷', '历史试卷', '地理试卷', '数学试卷', '生物试卷', '语文试卷']
+    root_dir = "G:\\final-www.shijuan1.com"
+    subject_dirs = sorted(os.listdir(root_dir))
+    for subject in subject_dirs:
+        if subject in subject_dirs_arr:
+            shijuan_dirs = sorted(os.listdir(root_dir + "\\" + subject))
 
-    files = sorted(os.listdir(word_dir))
-    for file in files:
-        if os.path.splitext(file)[1] in [".doc", ".docx"]:
-            print(file)
-            if os.path.splitext(file)[1] == ".docx":
-                # 将文件复制到doc2docx_dir目录
-                print("复制文件")
-                shutil.copyfile(word_dir + "\\" + file, doc2docx_dir + "\\" + file)
-            elif os.path.splitext(file)[1] == ".doc":
-                # 将doc文件转化为docx文件
-                print("转化文件")
-                doc2docx(word_dir + "\\" + file, doc2docx_dir + "\\" + os.path.splitext(file)[0] + ".docx")
-                time.sleep(3)
-            # 去除word页眉和页脚
-            doc2docx_file = doc2docx_dir + "\\" + os.path.splitext(file)[0] + ".docx"
-            finish_doc = finish_dir + "\\" + os.path.splitext(file)[0] + ".docx"
-            remove_header_footer(doc2docx_file, finish_doc)
+            for shijuan in shijuan_dirs:
+                word_dir = root_dir + "\\" + subject + "\\" + shijuan
+                finish_dir = root_dir + "\\" + subject + "\\" + shijuan + "_finish"
+                doc2docx_dir = root_dir + "\\" + subject + "\\" + shijuan + "_doc2docx"
+
+                if not os.path.exists(finish_dir):
+                    os.mkdir(finish_dir)
+
+                if not os.path.exists(doc2docx_dir):
+                    os.mkdir(doc2docx_dir)
+
+                files = sorted(os.listdir(word_dir))
+                for file in files:
+                    if os.path.splitext(file)[1] in [".doc", ".docx"]:
+                        print(file)
+                        if os.path.splitext(file)[1] == ".docx":
+                            # 将文件复制到doc2docx_dir目录
+                            print("复制文件")
+                            shutil.copyfile(word_dir + "\\" + file, doc2docx_dir + "\\" + file)
+                        elif os.path.splitext(file)[1] == ".doc":
+                            # 将doc文件转化为docx文件
+                            print("转化文件")
+                            doc2docx(word_dir + "\\" + file, doc2docx_dir + "\\" + os.path.splitext(file)[0] + ".docx")
+                        # 去除word页眉和页脚
+                        doc2docx_file = doc2docx_dir + "\\" + os.path.splitext(file)[0] + ".docx"
+                        finish_doc = finish_dir + "\\" + os.path.splitext(file)[0] + ".docx"
+                        if get_word_pages(doc2docx_file) >= 3:
+                            remove_header_footer(doc2docx_file, finish_doc)
