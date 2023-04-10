c5e10c32b160cdbd22fff07f726738055e06da33
周国伟 <zhouguowei@zhouguoweideMacBook-Pro.local>
2023-04-10 13:30:38 +0800

    异常处理

--- main_final_zuowenwang.py
+++ main_final_zuowenwang.py
@@ -29,36 +29,37 @@ def change_word_font(doc_file):
 def change_line_spacing(doc_file):
     doc = Document(doc_file)
     for p in doc.paragraphs:  # 循环处理每个段落
-        p.paragraph_format.line_spacing = 2  # 行距设置为3
+        p.paragraph_format.line_spacing = 1.5  # 行距设置为3
     doc.save(doc_file)
 
 
 number_dirs_arr = [
-    2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27
 ]
 
 if __name__ == '__main__':
     root_dir = "../www.zuowenwang.net"
-    number_dirs = sorted(os.listdir(root_dir))
-    for number_dir in number_dirs:
-        if number_dir in number_dirs_arr:
-            if os.path.isdir(root_dir + "/" + number_dir):
-                files = sorted(os.listdir(root_dir + "/" + number_dir))
-                for file in files:
-                    if os.path.splitext(file)[1] == ".docx":
-                        file_path = root_dir + "/" + number_dir + "/" + file
-                        print(file_path)
-                        file_finish_dir = root_dir + "/" + number_dir + "/"
-                        file_finish_dir = file_finish_dir.replace("www.zuowenwang.net", "finish.zuowenwang.net")
-                        if not os.path.exists(file_finish_dir):
-                            os.makedirs(file_finish_dir)
-                        file_finish_path = file_finish_dir + file.split("-")[1]
+    for number_dir in number_dirs_arr:
+        number_dir = str(number_dir)
+        files = sorted(os.listdir(root_dir + "/" + number_dir))
+        for file in files:
+            if os.path.splitext(file)[1] == ".docx":
+                file_path = root_dir + "/" + number_dir + "/" + file
+                print(file_path)
+                file_finish_dir = root_dir + "/" + number_dir + "/"
+                file_finish_dir = file_finish_dir.replace("www.zuowenwang.net", "finish.zuowenwang.net")
+                if not os.path.exists(file_finish_dir):
+                    os.makedirs(file_finish_dir)
+                file_finish_path = file_finish_dir + file.split("-")[1]
 
-                        # 删除页眉页脚
-                        remove_header_footer(file_path, file_finish_path)
+                try:
+                    # 删除页眉页脚
+                    remove_header_footer(file_path, file_finish_path)
 
-                        # 改变文档字体
-                        change_word_font(file_finish_path)
+                    # 改变文档字体
+                    change_word_font(file_finish_path)
 
-                        # 修改行距
-                        change_line_spacing(file_finish_path)
+                    # 修改行距
+                    change_line_spacing(file_finish_path)
+                except Exception as e:
+                    print(e)
