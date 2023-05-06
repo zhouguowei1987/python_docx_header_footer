56077c940739ae3bf4276cbbd7c1485165a8b106
729044963@qq.com <729044963@qq.com>
2023-05-06 10:23:20 +0800

    设置页眉

--- main_tikuvip_51test.py
+++ main_tikuvip_51test.py
@@ -2,13 +2,16 @@ import re
 import time
 from docx import Document
 from docx.shared import Pt
+from docx.shared import Cm
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import RGBColor  # 设置字体的颜色
 from docx.oxml.ns import qn
 from win32com import client as wc
 import os
 import shutil
 
 
-def remove_header_footer(doc, save_doc):
+def remove_and_set_header_footer(doc, save_doc):
     # doc：需要去页眉页脚的docx 文件
     # finish_doc： 需要另存为的新文件名
     document = Document(doc)
@@ -16,6 +19,20 @@ def remove_header_footer(doc, save_doc):
         section.different_first_page_header_footer = False
         section.header.is_linked_to_previous = True
         section.footer.is_linked_to_previous = True
+
+        section.header_distance = Cm(1.5)
+        section.footer_distance = Cm(1.75)
+        # 设置页眉
+        header = section.header  # 获取第一个节的页眉（所有的页眉都一致）
+        paragraph = header.paragraphs[0]  # 获取页眉的文字part
+        text = paragraph.add_run('年寒窗苦读日，只盼金榜题名时，祝你考试拿高分，鲤鱼跳龙门！加油！')
+        text.font.size = Pt(10)  # 页眉字体大小
+        text.font.color.rgb = RGBColor(255, 0, 0)
+        # text.bold = True  # 页眉字体是否加粗
+        text.font.name = 'Times New Roman'  # 控制是英文时的字体
+        text.element.rPr.rFonts.set(qn('w:eastAsia'), '微软雅黑')  # 控制是中文时的字体
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER  # 设置页眉居中
+
     document.save(save_doc)
 
 
@@ -63,14 +80,14 @@ def change_word_font(doc_file):
 
 
 def check_only_image(doc_file):
-    doc = Document(doc_file)
-    i = 0
-    for para in doc.paragraphs:
-        if i == 0 and para.text == "":
-            doc.save(doc_file)
+    try:
+        doc = Document(doc_file)
+        if len(doc.paragraphs) == 1 or len(doc.paragraphs) == 0:
             return True
-        i += 1
-    doc.save(doc_file)
+        doc.save(doc_file)
+    except Exception as e:
+        print(e)
+        return True
     return False
 
 
@@ -95,7 +112,7 @@ def doc2docx(in_file, out_file):
 
 if __name__ == '__main__':
     category_dirs_arr = ['自考', '专升本考试', '一级建造师考试', '小升初', '考研', '公务员考试', '高中会考', '高考',
-                         '二级建造师考试', '成人高考']
+                         '二级建造师考试', '成人高考', '事业单位招聘', '中考']
     root_dir = "G:\\tikuvip.51test.net"
     category_dirs = sorted(os.listdir(root_dir))
     for category in category_dirs:
@@ -127,8 +144,7 @@ if __name__ == '__main__':
                     if check_only_image(docx_file):
                         continue
                     finish_file = os.path.splitext(finish_file)[0] + "（精品真题）" + os.path.splitext(finish_file)[1]
-                    # 删除页眉页脚
-                    remove_header_footer(docx_file, finish_file)
+                    # 删除并设置页眉页脚
+                    remove_and_set_header_footer(docx_file, finish_file)
                     # 改变文档字体
                     change_word_font(finish_file)
-
