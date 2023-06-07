89bf807c2e66e673c56f9d7b255b99555565ce2e
729044963@qq.com <729044963@qq.com>
2023-06-07 10:56:44 +0800

    完结

--- main_final_hi138.py
+++ main_final_hi138.py
@@ -27,17 +27,22 @@ def remove_header_footer(doc, save_doc):
 def docx_remove_content(doc_file):
     # 定义需要去除及替换的内容
     content_to_removes = [
+        ['''论文类别：(.*?)
+上传时间：(.*?)
+论文作者：(.*?)
+论文版本：(.*?) (.*?) (.*?)''', ''],
         ['''免费论文下载中 http://www.hi138.com 　　''', '\n\r\t'],
         ['''免费论文下载中心 http://www.hi138.com''', ''],
         ['''免费论文下载中 http://www.hi138.com''', ''],
-        ['''
-''', ''],
         ['''免费论文下载中心讯：''', ''],
-        ['''声明：本论文来自免费论文下载中心：(.*?)，本站仅供大家学习、研究、参考之用，未取得作者授权严禁摘编、篡改、用作商业用途.''', '']
+        ['''代写论文网： ''', ''],
+        ['''声明：
+本论文来自免费论文下载中心：(.*?)
+免费论文下载中心（www.hi138.com）(.*?)，本站仅供大家学习、研究、参考之用，未取得作者授权严禁摘编、篡改、用作商业用途.''', '']
     ]
     # 打开doc文件
     doc = Document(doc_file)
-    doc.paragraphs[1].clear()
+    # doc.paragraphs[1].clear()
     # 遍历doc文件中的段落
     for para in doc.paragraphs:
         # 如果段落中包含需要去除的内容，使用正则表达式
@@ -63,11 +68,18 @@ def change_word_font(doc_file):
     doc.save(doc_file)
 
 
+def delete_blank_line(doc_file):
+    doc = Document(doc_file)
+    for p in doc.paragraphs:  # 循环处理每个段落
+        if len(p.text.replace("\n", "").replace(" ", "")) == 0:
+            p.clear()
+    doc.save(doc_file)
+
+
 def change_line_spacing(doc_file):
     doc = Document(doc_file)
     for p in doc.paragraphs:  # 循环处理每个段落
-        if p.paragraph_format.line_spacing:
-            p.paragraph_format.line_spacing = 1.5  # 行距设置为3
+        p.paragraph_format.line_spacing = 1.5  # 行距设置为3
     doc.save(doc_file)
 
 
@@ -106,7 +118,7 @@ if __name__ == '__main__':
     for file in files:
         if os.path.splitext(file)[1] == ".doc":
             file_path = root_dir + file
-            print(file_path+"==============")
+            print(file_path + "==============")
 
             docx_dir = "G:\\docx.hi138.com\\"
             if not os.path.exists(docx_dir):
@@ -134,8 +146,11 @@ if __name__ == '__main__':
                     # 过滤文档文字
                     docx_remove_content(finish_file)
 
+                    # 删除空白行
+                    delete_blank_line(finish_file)
+
                     # 改变文档字体
-                    change_word_font(finish_file)
+                    # change_word_font(finish_file)
 
                     # 修改行距
                     change_line_spacing(finish_file)
