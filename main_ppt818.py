0b84065649496f81e4c7153009366b35c7e33f4f
729044963@qq.com <729044963@qq.com>
2023-10-26 11:58:17 +0800

    处理ppt818文档

--- main_ppt818.py
+++ main_ppt818.py
@@ -1,31 +1,84 @@
-from pptx import Presentation
+import re
+import time
+from docx import Document
+from docx.shared import Pt
+from docx.shared import Cm
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
+from docx.shared import RGBColor  # 设置字体的颜色
+from docx.oxml.ns import qn
+from win32com import client as wc
 import os
+import shutil
+
+
+def change_word_font(doc_file):
+    # 打开doc文件
+    doc = Document(doc_file)
+    doc.styles['Normal'].font.name = u'Times New Roman'  # 设置西文字体
+    doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')  # 设置中文字体使用字体2->宋体
+    doc.save(doc_file)
+
+
+def remove_header_footer(doc_file):
+    # doc：需要去页眉页脚的docx 文件
+    doc = Document(doc_file)
+    for section in doc.sections:
+        section.different_first_page_header_footer = False
+        section.header.is_linked_to_previous = True
+        section.footer.is_linked_to_previous = True
+    doc.save(doc_file)
+
+
+def doc2docx(in_file, out_file):
+    try:
+        word = wc.Dispatch("Word.Application")
+        try:
+            print(in_file)
+            print(out_file)
+            doc = word.Documents.Open(in_file)
+            doc.SaveAs(out_file, 12, False, "", True, "", False, False, False, False)
+            print('转换成功')
+            doc.Close()
+            word.Quit()
+            return True
+        except Exception as e:
+            print(e)
+    except Exception as e:
+        print(e)
+    return False
 
 
 if __name__ == '__main__':
-    root_dir = "G:\\aaa"
-    files = sorted(os.listdir(root_dir))
-    for file in files:
-        file_path = root_dir + "\\" + file
-        print(file_path)
+    category_dirs_arr = ['个人简历', '工作总结', '公文通知', '合同范本', '行政管理', '演讲稿', '营销策划']
+    root_dir = "../www.ppt818.com/temp.www.ppt818.com"
+    category_dirs = sorted(os.listdir(root_dir))
+    for category in category_dirs:
+        if category in category_dirs_arr:
+            files = sorted(os.listdir(root_dir + "\\" + category))
+            for file in files:
+                file_path = root_dir + "\\" + category + "\\" + file
+                print(file_path)
+                docx_dir = "../www.ppt818.com/docx.www.ppt818.com"
+                if not os.path.exists(docx_dir):
+                    os.makedirs(docx_dir)
 
-        prs = Presentation(file_path)
+                docx_file = docx_dir + "\\" + category
 
-        slide_master = prs.slide_masters[1]
-        print(slide_master.name)
-        # slide_master.name = "New Name"
+                # 获取文件后缀
+                file_ext = os.path.splitext(file_path)[-1]
+                if file_ext == ".docx":
+                    # 已经是docx文件了，直接复制过去
+                    shutil.copy(file_path, docx_file)
+                else:
+                    with open(docx_file, 'w') as f:
+                        pass
+                    print("==========开始转化为docx==============")
+                    if not doc2docx(file_path, docx_file):
+                        continue
+                    print("==========转化完成==============")
 
-        # slide_master = prs.slide_masters[0]
-        # print(slide_master.name)
-        #
-        layout = prs.slide_layouts[0]
-        layout.name = "ababababa"
-        print(layout.name)
-        prs.save(root_dir + "\\" + "电子奖状模板.pptx")
-        # print(prs.slide_masters)
-        # slide_master = prs.slide_masters[0]
-        # print(slide_master.name)
-        # for slide_master in prs.slide_masters:
-        #     print(slide_master.name)
-
-
+                if os.path.exists(docx_file):
+                    # 改变文档字体
+                    change_word_font(docx_file)
