ef1088ab230c9bb2b74b7ca56164a775c566657b
729044963@qq.com <729044963@qq.com>
2023-12-05 13:56:40 +0800

    处理图片文档

--- main_shijuan1.py
+++ main_shijuan1.py
@@ -59,7 +59,23 @@ def doc2docx(in_file, out_file):
     except Exception as e:
         print(e)
     return False
-
+def check_only_image(doc_file):
+    try:
+        doc = Document(doc_file)
+        if len(doc.paragraphs) < 5:
+            return True
+        else:
+            i = 0
+            for para in doc.paragraphs:
+                if i == 4 and para.text == "":
+                    doc.save(doc_file)
+                    return True
+                i += 1
+        doc.save(doc_file)
+    except Exception as e:
+        print(e)
+        return True
+    return False
 
 def decompress_rar(rar_file_name, dir_name):
     """
@@ -101,7 +117,7 @@ if __name__ == '__main__':
     # exit()
 
     # category_dirs_arr = ['中考试卷', '高考试卷']
-    category_dirs_arr = ['中考试卷']
+    category_dirs_arr = ['高考试卷']
     root_dir = "G:\\www.shijuan1.com\\www.shijuan1.com"
     category_dirs = sorted(os.listdir(root_dir))
     for category in category_dirs:
@@ -185,6 +201,12 @@ if __name__ == '__main__':
                         print("==========转化完成==============")
 
                 if os.path.exists(docx_file):
+                    # 删除只包含图片
+                    if check_only_image(docx_file):
+                        # 删除图片文件
+                        os.remove(docx_file)
+                        continue
+
                     # 删除页眉页脚
                     if not remove_header_footer(docx_file):
                         os.remove(docx_file)
