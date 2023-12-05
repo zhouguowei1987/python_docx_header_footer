af71abe14398cb16fe3f566c4e74cbf7be469efe
729044963@qq.com <729044963@qq.com>
2023-12-05 08:32:51 +0800

    第一试卷网

--- main_shijuan1.py
+++ main_shijuan1.py
@@ -29,31 +29,17 @@ def change_word_font(doc_file):
 
 def remove_header_footer(doc):
     # doc：需要去页眉页脚的docx 文件
-    document = Document(doc)
-    for section in document.sections:
-        section.different_first_page_header_footer = False
-        section.header.is_linked_to_previous = True
-        section.footer.is_linked_to_previous = True
-    document.save(doc)
-
-
-def check_only_image(doc_file):
     try:
-        doc = Document(doc_file)
-        if len(doc.paragraphs) < 5:
-            return True
-        else:
-            i = 0
-            for para in doc.paragraphs:
-                if i == 4 and para.text == "":
-                    doc.save(doc_file)
-                    return True
-                i += 1
-        doc.save(doc_file)
+        document = Document(doc)
+        for section in document.sections:
+            section.different_first_page_header_footer = False
+            section.header.is_linked_to_previous = True
+            section.footer.is_linked_to_previous = True
+        document.save(doc)
+        return True
     except Exception as e:
         print(e)
-        return True
-    return False
+        return False
 
 
 def doc2docx(in_file, out_file):
@@ -98,23 +84,24 @@ def decompress_rar(rar_file_name, dir_name):
 if __name__ == '__main__':
     # 解压压缩包
     # category_rars_arr = ['中考试卷', '高考试卷']
-    category_rars_arr = ['高考试卷']
-    rar_root_dir = "G:\\www.rar_shijuan1.com"
-    rar_dirs = sorted(os.listdir(rar_root_dir))
-    for category_rar in category_rars_arr:
-        rar_files = sorted(os.listdir(rar_root_dir + "\\" + category_rar))
-        for rar_file in rar_files:
-            rar_file_path = rar_root_dir+ "\\" + category_rar + "\\" + rar_file
-            print("==========" + "开始解压" + rar_file_path + "==========")
-            try:
-                decompress_rar(rar_file_path, "G:\\www.shijuan1.com\\www.shijuan1.com\\" + category_rar)
-            except Exception as e:
-                print(e)
-                continue
-            print("==========" + "解压完成" + "==========")
-    exit()
+    # category_rars_arr = ['高考试卷']
+    # rar_root_dir = "G:\\www.rar_shijuan1.com"
+    # rar_dirs = sorted(os.listdir(rar_root_dir))
+    # for category_rar in category_rars_arr:
+    #     rar_files = sorted(os.listdir(rar_root_dir + "\\" + category_rar))
+    #     for rar_file in rar_files:
+    #         rar_file_path = rar_root_dir+ "\\" + category_rar + "\\" + rar_file
+    #         print("==========" + "开始解压" + rar_file_path + "==========")
+    #         try:
+    #             decompress_rar(rar_file_path, "G:\\www.shijuan1.com\\www.shijuan1.com\\" + category_rar)
+    #         except Exception as e:
+    #             print(e)
+    #             continue
+    #         print("==========" + "解压完成" + "==========")
+    # exit()
 
-    category_dirs_arr = ['中考试卷', '高考试卷']
+    # category_dirs_arr = ['中考试卷', '高考试卷']
+    category_dirs_arr = ['高考试卷']
     root_dir = "G:\\www.shijuan1.com\\www.shijuan1.com"
     category_dirs = sorted(os.listdir(root_dir))
     for category in category_dirs:
@@ -124,7 +111,11 @@ if __name__ == '__main__':
                 file_path = root_dir + "\\" + category + "\\" + file
                 print(file_path)
 
-                # 查看一下是否是文件夹，如果是文件夹，则将文件移出
+                # 文件名称小于20，则删除
+                # if len(file) < 20:
+                #     os.remove(file_path)
+
+                # # 查看一下是否是文件夹，如果是文件夹，则将文件移出
                 # if os.path.isdir(file_path):
                 #     child_files = sorted(os.listdir(file_path))
                 #     for child_file in child_files:
@@ -133,13 +124,23 @@ if __name__ == '__main__':
                 #             continue
                 #         src_child_file_path = file_path + "\\" + child_file
                 #         dst_child_file_path = root_dir + "\\" + category + "\\" + child_file
-                #         os.rename(src_child_file_path, dst_child_file_path)
+                #         try:
+                #             os.rename(src_child_file_path, dst_child_file_path)
+                #         except WindowsError:
+                #             os.remove(dst_child_file_path)
+                #             os.rename(src_child_file_path, dst_child_file_path)
 
                 docx_dir = "G:\\www.shijuan1.com\\docx.www.shijuan1.com" + "\\" + category
                 if not os.path.exists(docx_dir):
                     os.makedirs(docx_dir)
 
-                docx_file = docx_dir + "\\" + file.lower().replace(os.path.splitext(file)[1], ".docx")
+                sub_file = file
+                left_flag_index = file.find("【")
+                right_flag_index = file.find("】")
+                if left_flag_index == 0 and right_flag_index != -1:
+                    # 文档名称以“【”开头，以“】”结尾，则替换名称
+                    sub_file = file[right_flag_index + 1:]
+                docx_file = docx_dir + "\\" + sub_file.lower().replace(os.path.splitext(sub_file)[1], ".docx")
                 docx_file = docx_file.strip()
                 docx_file = docx_file.replace("（", "(").replace("）", ")")
                 docx_file = docx_file.replace("精品解析：", "")
@@ -183,8 +184,10 @@ if __name__ == '__main__':
                 if os.path.exists(docx_file):
                     # 删除页眉页脚
                     if not remove_header_footer(docx_file):
+                        os.remove(docx_file)
                         continue
 
                     # 改变文档字体
                     if not change_word_font(docx_file):
+                        os.remove(docx_file)
                         continue
