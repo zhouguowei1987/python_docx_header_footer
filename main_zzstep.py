fd780964a9ed557cabe394d368f3f5cdb6257d8c
zhouguowei <729044963@qq.com>
2023-12-19 09:47:00 +0800

    提交

--- main_zzstep.py
+++ main_zzstep.py
@@ -6,9 +6,10 @@ from docx.shared import Cm
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml.ns import nsdecls
 from docx.oxml import parse_xml
+from docx2pdf import convert
 from docx.shared import RGBColor  # 设置字体的颜色
 from docx.oxml.ns import qn
-from win32com import client as wc
+# from win32com import client as wc
 import os
 import shutil
 
@@ -58,44 +59,44 @@ def remove_header_footer(doc_file):
         return False
 
 
-def doc2docx(in_file, out_file):
-    try:
-        word = wc.Dispatch("Word.Application")
-        try:
-            print(in_file)
-            print(out_file)
-            doc = word.Documents.Open(in_file)
-            if doc.ProtectionType == 1:
-                print('文档加密，转换失败')
-                doc.Close()
-                word.Quit()
-                return False
-            else:
-                doc.SaveAs(out_file, 12, False, "", True, "", False, False, False, False)
-                print('转换成功')
-                doc.Close()
-                word.Quit()
-                return True
-        except Exception as e:
-            print(e)
-    except Exception as e:
-        print(e)
-    return False
+# def doc2docx(in_file, out_file):
+#     try:
+#         word = wc.Dispatch("Word.Application")
+#         try:
+#             print(in_file)
+#             print(out_file)
+#             doc = word.Documents.Open(in_file)
+#             if doc.ProtectionType == 1:
+#                 print('文档加密，转换失败')
+#                 doc.Close()
+#                 word.Quit()
+#                 return False
+#             else:
+#                 doc.SaveAs(out_file, 12, False, "", True, "", False, False, False, False)
+#                 print('转换成功')
+#                 doc.Close()
+#                 word.Quit()
+#                 return True
+#         except Exception as e:
+#             print(e)
+#     except Exception as e:
+#         print(e)
+#     return False
 
 
 if __name__ == '__main__':
-    category_dirs_arr = ['物理', '英语', '语文']
-    root_dir = "G:\\www2.zzstep.com\\www2.zzstep.com\\高中"
+    category_dirs_arr = ['数学',	'英语', '音乐', '道德与法治', '美术', '语文', '信息技术']
+    root_dir = "../www2.zzstep.com/www2.zzstep.com/小学"
     category_dirs = sorted(os.listdir(root_dir))
     for category in category_dirs:
         if category in category_dirs_arr:
-            files = sorted(os.listdir(root_dir + "\\" + category))
+            files = sorted(os.listdir(root_dir + "/" + category))
             for file in files:
                 print(file)
 
                 file_path = root_dir + "\\" + category + "\\" + file
                 print(file_path)
-                docx_dir = "G:\\www2.zzstep.com\\docx.zzstep.com\\高中\\" + category
+                docx_dir = "../www2.zzstep.com/docx.zzstep.com/小学/" + category
                 if not os.path.exists(docx_dir):
                     os.makedirs(docx_dir)
 
@@ -116,41 +117,58 @@ if __name__ == '__main__':
                     print("文档标题不包含分类名称，跳过")
                     continue
 
+                # docx文件已存在，跳过继续
                 if os.path.exists(docx_file):
-                    print("文件已存在，跳过")
-                    continue
+                    # continue
 
-                # 获取文件后缀
-                file_ext = os.path.splitext(file_path)[-1]
-                if file_ext == ".docx":
-                    # 已经是docx文件了，直接复制过去
-                    shutil.copy(file_path, docx_file)
-                else:
-                    with open(docx_file, 'w') as f:
-                        pass
-                    print("==========开始转化为docx==============")
-                    if not doc2docx(file_path, docx_file):
-                        print("转换失败，删除文件")
-                        os.remove(docx_file)
-                        continue
-                    print("==========转化完成==============")
+                    finish_dir = "../www2.zzstep.com/finish.zzstep.com/小学/" + category
+                    if not os.path.exists(finish_dir):
+                        os.makedirs(finish_dir)
+                    # 将docx文件转化为pdf
+                    finish_file = docx_file.replace("docx.", "finish.").replace(".docx", ".pdf")
+                    if not os.path.exists(finish_file):
+                        # 将docx转化为pdf
+                        with open(finish_file, "w") as f:
+                            # 将 Word 文档转换为 PDF
+                            try:
+                                print("==========开始转化为pdf==============")
+                                convert(docx_file, finish_file)
+                                print("转换成功！")
+                            except Exception as e:
+                                print("转换失败：", str(e))
 
-                if os.path.exists(docx_file):
+                # if not os.path.exists(docx_file):
+                #     # 获取文件后缀
+                #     file_ext = os.path.splitext(file_path)[-1]
+                #     if file_ext == ".docx":
+                #         # 已经是docx文件了，直接复制过去
+                #         shutil.copy(file_path, docx_file)
+                #     else:
+                #         with open(docx_file, 'w') as f:
+                #             pass
+                #         print("==========开始转化为docx==============")
+                #         if not doc2docx(file_path, docx_file):
+                #             print("转换失败，删除文件")
+                #             os.remove(docx_file)
+                #             continue
+                #         print("==========转化完成==============")
 
-                    # 删除页眉页脚
-                    if not remove_header_footer(docx_file):
-                        print("删除页眉页脚失败，删除文件")
-                        os.remove(docx_file)
-                        continue
-
-                    # 删除只包含图片
-                    if check_only_image(docx_file):
-                        print("文档只包含图片，删除文件")
-                        os.remove(docx_file)
-                        continue
-
-                    # 改变文档字体
-                    if not change_word_font(docx_file):
-                        print("改变文档字体失败，删除文件")
-                        os.remove(docx_file)
-                        continue
+                # if os.path.exists(docx_file):
+                #
+                #     # 删除页眉页脚
+                #     if not remove_header_footer(docx_file):
+                #         print("删除页眉页脚失败，删除文件")
+                #         os.remove(docx_file)
+                #         continue
+                #
+                #     # 删除只包含图片
+                #     if check_only_image(docx_file):
+                #         print("文档只包含图片，删除文件")
+                #         os.remove(docx_file)
+                #         continue
+                #
+                #     # 改变文档字体
+                #     if not change_word_font(docx_file):
+                #         print("改变文档字体失败，删除文件")
+                #         os.remove(docx_file)
+                #         continue
