b8abff9c73b43a2c19421708aaeaabca692933e5
729044963@qq.com <729044963@qq.com>
2023-12-19 17:16:28 +0800

    简历设计处理

---
+++ main_jianlisheji.py
@@ -0,0 +1,127 @@
+import re
+import time
+from docx import Document
+from docx.shared import Pt
+from docx.shared import Cm
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
+from docx.shared import RGBColor  # 设置字体的颜色
+from docx.oxml.ns import qn
+from win32com import client as wc
+import os
+import shutil
+
+
+def change_word_font(doc_file):
+    # 打开doc文件
+    doc = Document(doc_file)
+    doc.styles['Normal'].font.name = u'Times New Roman'  # 设置西文字体
+    doc.styles['Normal']._element.rPr.rFonts.set(qn('w:eastAsia'), u'微软雅黑')  # 设置中文字体使用字体2->宋体
+    doc.save(doc_file)
+
+
+def docx_remove_content(doc_file):
+    # 定义需要去除及替换的内容
+    content_to_removes = [
+        ['''简历设计网''', '个人简历'],
+        ['''jianlisheji''', 'XXXXXX'],
+        ['''Jianlisheji''', 'XXXXXX'],
+    ]
+    # 打开doc文件
+    doc = Document(doc_file)
+    doc.paragraphs[1].clear()
+    # 遍历doc文件中的段落
+    for para in doc.paragraphs:
+        # 如果段落中包含需要去除的内容，使用正则表达式
+        for content_to_remove in content_to_removes:
+            if re.search(content_to_remove[0], para.text):
+                para.text = re.sub(content_to_remove[0], content_to_remove[1], para.text)
+
+    doc.save(doc_file)
+
+
+def remove_header_footer(doc):
+    # doc：需要去页眉页脚的docx 文件
+    try:
+        document = Document(doc)
+        for section in document.sections:
+            section.different_first_page_header_footer = False
+            section.header.is_linked_to_previous = True
+            section.footer.is_linked_to_previous = True
+        document.save(doc)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
+def doc2docx(in_file, out_file):
+    try:
+        word = wc.Dispatch("Word.Application")
+        try:
+            print(in_file)
+            print(out_file)
+            doc = word.Documents.Open(in_file)
+            doc.SaveAs(out_file, 12, False, "", True, "", False, False, False, False)
+            print('转换成功')
+            doc.Close()
+            word.Quit()
+            return True
+        except Exception as e:
+            print(e)
+    except Exception as e:
+        print(e)
+    return False
+
+
+if __name__ == '__main__':
+    root_dir = "F:\\workspace\\www.jianlisheji.com\\www.jianlisheji.com"
+    files = sorted(os.listdir(root_dir))
+    for file in files:
+        file_path = root_dir + "\\" + file
+        print(file_path)
+        docx_dir = "F:\\workspace\\www.jianlisheji.com\\docx.jianlisheji.com"
+        if not os.path.exists(docx_dir):
+            os.makedirs(docx_dir)
+
+        docx_file = docx_dir + "\\" + file.lower().replace(os.path.splitext(file)[1], ".docx")
+        docx_file = docx_file.replace(" ", "")
+        docx_file = docx_file.replace("word", "")
+        docx_file = docx_file.replace("Word", "")
+
+        # 获取文件后缀
+        file_ext = os.path.splitext(file_path)[-1]
+        if file_ext == ".docx":
+            # 已经是docx文件了，直接复制过去
+            shutil.copy(file_path, docx_file)
+        else:
+            with open(docx_file, 'w') as f:
+                pass
+            print("==========开始转化为docx==============")
+            if not doc2docx(file_path, docx_file):
+                os.remove(docx_file)
+                continue
+            print("==========转化完成==============")
+
+        if os.path.exists(docx_file):
+            # 改变文档文字
+            if not docx_remove_content(docx_file):
+                # 删除原文件
+                os.remove(file_path)
+                os.remove(docx_file)
+                continue
+
+            # 删除页眉页脚
+            if not remove_header_footer(docx_file):
+                # 删除原文件
+                os.remove(file_path)
+                os.remove(docx_file)
+                continue
+
+            # 改变文档字体
+            if not change_word_font(docx_file):
+                # 删除原文件
+                os.remove(file_path)
+                os.remove(docx_file)
+                continue
