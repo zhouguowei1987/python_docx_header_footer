61c6289925e2305781c090c496bddd678a425831
zhouguowei1987 <729044963@qq.com>
2024-12-25 22:24:35 +0800

    提交

--- main_topedu.py
+++ main_topedu.py
@@ -14,39 +14,23 @@ import shutil
 
 def remove_header_footer(doc):
     # doc：需要去页眉页脚的docx 文件
-    document = Document(doc)
-    for section in document.sections:
-        section.different_first_page_header_footer = False
-        section.header.is_linked_to_previous = True
-        section.footer.is_linked_to_previous = True
-    document.save(doc)
-
-
-def check_only_image(doc_file):
     try:
-        doc = Document(doc_file)
-        if len(doc.paragraphs) < 5:
-            return True
-        else:
-            i = 0
-            for para in doc.paragraphs:
-                if i == 4 and para.text == "":
-                    doc.save(doc_file)
-                    return True
-                i += 1
-        doc.save(doc_file)
+        document = Document(doc)
+        for section in document.sections:
+            section.different_first_page_header_footer = False
+            section.header.is_linked_to_previous = True
+            section.footer.is_linked_to_previous = True
+        document.save(doc)
+        return True
     except Exception as e:
         print(e)
-        return True
-    return False
+        return False
 
 
 def doc2docx(in_file, out_file):
     try:
         word = wc.Dispatch("Word.Application")
         try:
-            print(in_file)
-            print(out_file)
             doc = word.Documents.Open(in_file)
             doc.SaveAs(out_file, 12, False, "", True, "", False, False, False, False)
             print('转换成功')
@@ -62,7 +46,7 @@ def doc2docx(in_file, out_file):
 
 if __name__ == '__main__':
     category_dirs_arr = ['高考真题', '中考真题']
-    root_dir = "G:\\topedu.ybep.com.cn"
+    root_dir = "E:\\workspace\\topedu.ybep.com.cn\\topedu.ybep.com.cn"
     category_dirs = sorted(os.listdir(root_dir))
     for category in category_dirs:
         if category in category_dirs_arr:
@@ -71,7 +55,7 @@ if __name__ == '__main__':
                 file_path = root_dir + "\\" + category + "\\" + file
                 print(file_path)
 
-                docx_dir = "G:\\docx.topedu.ybep.com.cn" + "\\" + category
+                docx_dir = "E:\\workspace\\topedu.ybep.com.cn\\20241225\\docx.topedu.ybep.com.cn\\" + category
                 if not os.path.exists(docx_dir):
                     os.makedirs(docx_dir)
 
@@ -92,39 +76,49 @@ if __name__ == '__main__':
                 docx_file = docx_file.replace("(word版，有答案)", "")
                 docx_file = docx_file.replace("(文字版-含答案)", "")
                 docx_file = docx_file.replace("word版", "")
+                docx_file = docx_file.replace("word", "")
+                docx_file = docx_file.replace("()", "")
                 docx_file = docx_file.replace("-", "")
                 docx_file = docx_file.replace(",", "")
                 docx_file = docx_file.replace("，", "")
+                print(docx_file)
 
                 if not os.path.exists(docx_file):
-                    print("==========开始转化为docx==============")
-                    if not doc2docx(file_path, docx_file):
+                    # 获取文件后缀
+                    file_ext = os.path.splitext(file_path)[-1]
+                    if file_ext == ".docx":
+                        # 已经是docx文件了，直接复制过去
+                        shutil.copy(file_path, docx_file)
+                    else:
+                        with open(docx_file, 'w') as f:
+                            pass
+                        print("==========开始转化为docx==============")
+                        if not doc2docx(file_path, docx_file):
+                            # 删除原文件
+                            # os.remove(file_path)
+                            os.remove(docx_file)
+                            continue
+                        print("==========转化完成==============")
+
+                if os.path.exists(docx_file):
+                    # 删除页眉页脚
+                    if not remove_header_footer(docx_file):
+                        # 删除原文件
+                        # os.remove(file_path)
+                        os.remove(docx_file)
                         continue
-                    print("==========转化完成==============")
-                else:
-                    # 已经是docx文件了，直接复制过去
-                    shutil.copy(file_path, docx_file)
 
-                # 删除并设置页眉页脚
-                # remove_header_footer(docx_file)
-
-                finish_dir = "G:\\finish.topedu.ybep.com.cn" + "\\" + category
+                finish_dir = "E:\\workspace\\topedu.ybep.com.cn\\20241225\\finish.topedu.ybep.com.cn\\" + category
                 if not os.path.exists(finish_dir):
                     os.makedirs(finish_dir)
                 finish_file = docx_file.replace("docx.", "finish.").replace(".docx", ".pdf")
-
+                print(finish_file)
                 if not os.path.exists(finish_file):
-                    # 删除只包含图片
-                    if check_only_image(docx_file):
-                        # 删除图片文件
-                        print("删除文件")
-                        os.remove(file_path)
-                        os.remove(docx_file)
-                        continue
                     # 将docx转化为pdf
                     with open(finish_file, "w") as f:
                         # 将 Word 文档转换为 PDF
                         try:
+                            print("==========开始转化为pdf==============")
                             convert(docx_file, finish_file)
                             print("转换成功！")
                         except Exception as e:
