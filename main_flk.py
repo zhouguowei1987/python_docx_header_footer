e718fd1edc67e75235c7c55d7191a86eb86df85b
zhouguowei1987 <729044963@qq.com>
2025-09-11 15:55:26 +0800

    提交

--- main_flk.py
+++ main_flk.py
@@ -138,3 +138,5 @@ if __name__ == '__main__':
                         print("转换成功！")
                     except Exception as e:
                         print("转换失败：", str(e))
+        # 删除temp文件夹文件
+        os.remove(file_path)
